225cbe6a568699c3de912829b836320de2d2d8a6
Dinamiet <iceman.sm39@gmail.com>
2021-05-02 17:27:17 +0200

    Code formatting

--- include/task_scheduler.h
+++ include/task_scheduler.h
@@ -1,43 +1,45 @@
 #ifndef __TASK_SCHEDULER__
 #define __TASK_SCHEDULER__
 
-#include <stdint.h>
-
 #include "bufferedlist.h"
 
-typedef enum {
+#include <stdint.h>
+
+typedef enum
+{
 	RecurringTask,
 	SingleShotTask
 } TaskTypes;
 
-typedef enum {
+typedef enum
+{
 	InactiveTask,
 	ActiveTask
 } TaskStatus;
 
 typedef struct
 {
-	uint32_t Name;
-	TaskTypes Type;
+	uint32_t   Name;
+	TaskTypes  Type;
 	TaskStatus Status;
-	uint32_t Period;
-	uint32_t LastTimestep;
+	uint32_t   Period;
+	uint32_t   LastTimestep;
 	void (*Callback)(void*);
 	void* Data;
 } Task;
 
 typedef struct
 {
-	Task* Current;
+	Task*		 Current;
 	BufferedList Tasks;
 } TaskList;
 
-void TaskScheduler_Init(TaskList* taskList, Task* buffer, uint32_t size);
-Task* TaskScheduler_CreateRetriggerTask(TaskList* list, void (*callback)(void*), void* data,  uint32_t period);
-Task* TaskScheduler_CreateSingleShotTask(TaskList*list, void (*callback)(void*), void* data, uint32_t delay);
-void TaskScheduler_ChangeTaskStatus(Task* task, TaskStatus status);
-void TaskScheduler_ChangeTaskPeriod(Task* task, uint32_t period);
-void TaskScheduler_ChangeTaskCallback(Task* task, void (*callback)(void*), void* data);
+void  TaskScheduler_Init(TaskList* taskList, Task* buffer, uint32_t size);
+Task* TaskScheduler_CreateRetriggerTask(TaskList* list, void (*callback)(void*), void* data, uint32_t period);
+Task* TaskScheduler_CreateSingleShotTask(TaskList* list, void (*callback)(void*), void* data, uint32_t delay);
+void  TaskScheduler_ChangeTaskStatus(Task* task, TaskStatus status);
+void  TaskScheduler_ChangeTaskPeriod(Task* task, uint32_t period);
+void  TaskScheduler_ChangeTaskCallback(Task* task, void (*callback)(void*), void* data);
 // TODO: Remove task from the list
 void TaskScheduler_RunNextTask();
 
--- src/task_scheduler.c
+++ src/task_scheduler.c
@@ -3,63 +3,57 @@
 void TaskScheduler_Init(TaskList* taskList, Task* buffer, uint32_t size)
 {
 	BufferedList_Init(&taskList->Tasks, (Node*)buffer, sizeof(Task), size);
-	taskList->Current= NULL;
+	taskList->Current = NULL;
 }
 
-Task* TaskScheduler_CreateRetriggerTask(TaskList* list, void (*callback)(void*), void *data, uint32_t period)
+Task* TaskScheduler_CreateRetriggerTask(TaskList* list, void (*callback)(void*), void* data, uint32_t period)
 {
-	Task* newTask= (Task*)BufferedList_LinkTail(&list->Tasks);
+	Task* newTask = (Task*)BufferedList_LinkTail(&list->Tasks);
 	if (newTask)
 	{
-		newTask->Name= 0;
-		newTask->Type=RecurringTask;
-		newTask->Status= ActiveTask;
-		newTask->Period= period;
-		newTask->LastTimestep= 0;
-		newTask->Callback= callback;
-		newTask->Data= data;
+		newTask->Name		  = 0;
+		newTask->Type		  = RecurringTask;
+		newTask->Status		  = ActiveTask;
+		newTask->Period		  = period;
+		newTask->LastTimestep = 0;
+		newTask->Callback	  = callback;
+		newTask->Data		  = data;
 	}
 
 	if (list->Current == NULL)
-		list->Current= newTask;
+		list->Current = newTask;
 
 	return newTask;
 }
 // TODO: This function is very close to other create - create 'private' function which does the initialization
-Task* TaskScheduler_CreateSingleShotTask(TaskList*list, void (*callback)(void*), void* data, uint32_t delay)
+Task* TaskScheduler_CreateSingleShotTask(TaskList* list, void (*callback)(void*), void* data, uint32_t delay)
 {
-	Task* newTask= (Task*)BufferedList_LinkTail(&list->Tasks);
+	Task* newTask = (Task*)BufferedList_LinkTail(&list->Tasks);
 	if (newTask)
 	{
-		newTask->Name= 0;
-		newTask->Type=SingleShotTask;
-		newTask->Status= ActiveTask;
-		newTask->Period= delay;
-		newTask->LastTimestep= 0;
-		newTask->Callback= callback;
-		newTask->Data= data;
+		newTask->Name		  = 0;
+		newTask->Type		  = SingleShotTask;
+		newTask->Status		  = ActiveTask;
+		newTask->Period		  = delay;
+		newTask->LastTimestep = 0;
+		newTask->Callback	  = callback;
+		newTask->Data		  = data;
 	}
 
 	if (list->Current == NULL)
-		list->Current= newTask;
+		list->Current = newTask;
 
 	return newTask;
 }
 
-void TaskScheduler_ChangeTaskStatus(Task* task, TaskStatus status)
-{
-	task->Status = status;
-}
+void TaskScheduler_ChangeTaskStatus(Task* task, TaskStatus status) { task->Status = status; }
 
-void TaskScheduler_ChangeTaskPeriod(Task* task, uint32_t period)
-{
-	task->Period = period;
-}
+void TaskScheduler_ChangeTaskPeriod(Task* task, uint32_t period) { task->Period = period; }
 
 void TaskScheduler_ChangeTaskCallback(Task* task, void (*callback)(void*), void* data)
 {
-	task->Callback= callback;
-	task->Data= data;
+	task->Callback = callback;
+	task->Data	   = data;
 }
 
 void TaskScheduler_RunNextTask()
